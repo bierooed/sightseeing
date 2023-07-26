080fc04fffe02808b8cdac3ba46fe712f9cb87c9
bierooed <hov.hovanes13@gmail.com>
2023-07-27 02:06:14 +0400

    Load image classification model and write function which handle transfered image file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
 import "./App.css";
 
+import * as tf from "@tensorflow/tfjs";
+import { useEffect, useRef, useState } from "react";
+import { loadModel, imageFormatting } from "tm-image-model";
+
 function App() {
-  return <>Sight seeing</>;
+  const [model, setModel] = useState();
+  const [image, setImage] = useState();
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    async function handleModel() {
+      const url = "https://teachablemachine.withgoogle.com/models/orzfSu2sE/";
+      setModel(await loadModel(url));
+    }
+
+    handleModel();
+  }, []);
+
+  async function handleImage(imageFile) {
+    const image = await imageFormatting(imageFile, canvasRef);
+    setImage(image);
+  }
+
+  return (
+    <>
+      {!!model ? (
+        <div>
+          <canvas ref={canvasRef} />
+        </div>
+      ) : (
+        "Loading..."
+      )}
+    </>
+  );
 }
 
 export default App;
